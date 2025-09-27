f3bb586da9865421bc25aedcf6a542f498142cf6
Mario Mendez <mendezmario2018@gmail.com>
2025-09-27 14:39:47 -0500

    corrected

--- Assignment 5/turtle_triangle.py
+++ Assignment 5/turtle_triangle.py
@@ -4,28 +4,31 @@ def triangle(points, color):
     turtle.fillcolor(color)
     turtle.begin_fill()
     turtle.penup()
-    turtle.goto(points[0])   
+    turtle.goto(points[0])
     turtle.pendown()
-    turtle.goto(points[1])   
-    turtle.goto(points[2])  
-    turtle.goto(points[0])   
+    turtle.goto(points[1])
+    turtle.goto(points[2])
+    turtle.goto(points[0])
     turtle.end_fill()
 
 def main():
-    turtle.speed(3)  
-    
-    # Triangle 1
-    triangle([(0, 0), (100, 0), (50, 100)], "red")
-    
-    # Triangle 2
-    triangle([(-150, -50), (-50, -50), (-100, 50)], "blue")
-    
-    # Triangle 3
-    triangle([(150, -50), (250, -50), (200, 50)], "green")
+    turtle.speed(1)
 
+    # Points
+    A = (0, 0)
+    B = (100, 0)
+    D = (-100, 0)    
+    C = (0, -150)   
+    F = (-100, -300)
+    G = (100, -300)
+
+   
+    triangle([A, D, C], "green")
+    triangle([A, B, C], "red")
+    triangle([C, G ,F], "blue")
+
+    turtle.hideturtle()
     turtle.done()
 
 if __name__ == "__main__":
     main()
-
-    
