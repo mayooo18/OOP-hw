5bd842e2c1349f6ea1db28a7e6b5dd46d19771ab
Mario Mendez <mendezmario2018@gmail.com>
2025-10-08 15:34:03 -0500

    added try except handlers

--- Midterm_Exam/midterm/brit2interCalc.py
+++ Midterm_Exam/midterm/brit2interCalc.py
@@ -7,25 +7,44 @@ def menuoptions():
 
 
 
-#function that processes the request
+#function that processes the request and uses try except for value errors
 def loopformenu():
     active = True
     while active == True:
         menuoptions()
+        try:
+            choice = int(input("Enter your choice: "))
+        except ValueError:
+            print("invalid choice only enter number (0-3)")
+            continue
 
-        choice = int(input("Enter your choice: "))
         if choice == 1:
-            pounds = float(input("Enter weight in Pounds: "))
-            kilograms = pounds * 0.453592
-            print(f"{pounds} Pounds is equal to {kilograms:.2f} Kilograms\n")
+            try:
+                pounds = float(input("Enter weight in Pounds: "))
+                kilograms = pounds * 0.453592
+                print(f"{pounds} Pounds is equal to {kilograms:.2f} Kilograms\n")
+            except ValueError:
+                print("Enter Valid Number")
+
+
+
         elif choice == 2:
-            ounces = float(input("Enter weight in Ounces: "))
-            grams = ounces * 28.3495
-            print(f"{ounces} Ounces is equal to {grams:.2f} Grams\n")
+            try:
+                ounces = float(input("Enter weight in Ounces: "))
+                grams = ounces * 28.3495
+                print(f"{ounces} Ounces is equal to {grams:.2f} Grams\n")
+            except ValueError:
+                print("Enter Valid Number")
+
+        
         elif choice == 3:
-            inches = float(input("Enter length in Inches: "))
-            centimeters = inches * 2.54
-            print(f"{inches} Inches is equal to {centimeters:.2f} Centimeters\n")
+            try:
+                inches = float(input("Enter length in Inches: "))
+                centimeters = inches * 2.54
+                print(f"{inches} Inches is equal to {centimeters:.2f} Centimeters\n")
+            except ValueError:
+                print("Enter Valid Number")
+
         
         elif choice == 0:
             active = False
@@ -34,7 +53,7 @@ def loopformenu():
 
         else:
             print("Invalid choice. Please try again.")
-     
+    
 
 #function that applies all of the above.
 #  if i were to expand on it i would make them into classes and just leave the main here 
